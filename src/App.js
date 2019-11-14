342c70caf516832c1dcb9a1f4ff389d7a8163532
Jesse Strode <jesse_strode@condenast.com>
2019-11-14 16:09:21 -0500

    initial commit after hackathon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import lodash from 'lodash'
+import YAML from 'json2yaml'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'Enter properties here',
+            properties: "nothing yet"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        let lines = this.state.value.split(/\r?\n/);
+        let properties = {};
+        for (let line of lines) {
+
+            console.log(line);
+
+            if (line.charAt(0) && line.charAt(0) !== '#') {
+                let parsedLine = line.split(/=(.+)/);
+                let pieces = parsedLine[0].split('.');
+                let value = parsedLine[1];
+                lodash.setWith(properties, pieces, value, Object);
+            }
+        }
+        let ymlText = YAML.stringify(properties);
+
+        // alert('results: \n' + ymlText);
+        this.setState({properties: ymlText});
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            properties:
+                            <textarea value={this.state.value} onChange={this.handleChange}/>
+                        </label>
+                        <input type="submit" value="Submit"/>
+                    </form>
+
+                </header>
+                <div>
+
+                    <SyntaxHighlighter language="yaml" style={docco}>
+                        {this.state.properties}
+                    </SyntaxHighlighter>
+
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
